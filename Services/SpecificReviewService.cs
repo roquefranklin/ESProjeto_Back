90501978e6d4c748cdd2682388bb3f8ab3e93bb3
Pedro Paulo <pvianna@ersoft.com.br>
2024-09-07 18:07:59 -0300

    fix: source code de service para Avaliação Específica

---
+++ Services/SpecificReviewService.cs
@@ -0,0 +1,38 @@
+﻿using ESProjeto_Back.Data.Dtos;
+using ESProjeto_Back.Interfaces;
+using ESProjeto_Back.Repositories.Interface;
+
+namespace ESProjeto_Back.Services
+{
+    public class SpecificReviewService : ISpecificReviewService
+    {
+
+        private readonly ISpecificReviewRepository _specificReviewController;
+
+        public SpecificReviewService(ISpecificReviewRepository specificReviewService)
+        {
+            _specificReviewController = specificReviewService;
+        }
+
+        public CreateSpecificReviewDto CreateSpecificReview(CreateSpecificReviewDto specificReviewDto)
+        {
+            var newSpecificReview = _specificReviewController.CreateSpecificReview(specificReviewDto);
+
+            return newSpecificReview;
+        }
+
+        public List<GetSpecificReviewDto> GetAllSpecificReviewsFromReveiw(Guid reviewId)
+        {
+            var specificReviews = _specificReviewController.GetAllSpecificReveiwsFromReview(reviewId);
+
+            return specificReviews;
+        }
+
+        public GetSpecificReviewDto GetSpecificReview(Guid id)
+        {
+            var specificReview = _specificReviewController.GetSpecificReview(id);
+
+            return specificReview;
+        }
+    }
+}
